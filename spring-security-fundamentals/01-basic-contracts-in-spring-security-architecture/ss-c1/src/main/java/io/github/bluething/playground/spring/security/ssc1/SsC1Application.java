9a152a4ea7673c0d0c2fb28aa34d150a92c4c48b
bluething <habib.gunadarma@gmail.com>
2022-04-03 11:05:32 +0700

    Init ss-c1

---
+++ spring-security-fundamentals/01-basic-contracts-in-spring-security-architecture/ss-c1/src/main/java/io/github/bluething/playground/spring/security/ssc1/SsC1Application.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.security.ssc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsC1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsC1Application.class, args);
+    }
+
+}
---
+++ spring-security-fundamentals/01-basic-contracts-in-spring-security-architecture/ss-c1/src/test/java/io/github/bluething/playground/spring/security/ssc1/SsC1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.security.ssc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsC1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
