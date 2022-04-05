f0dd4cbca67374bf40161e7f8a4f51623677a12f
bluething <habib.gunadarma@gmail.com>
2022-04-05 07:19:48 +0700

    Init the ss-c2 module

---
+++ spring-security-fundamentals/02-Implementing-the-UserDetailsService/ss-c2/src/test/java/io/github/bluething/playground/spring/security/ssc2/SsC2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.security.ssc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsC2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
