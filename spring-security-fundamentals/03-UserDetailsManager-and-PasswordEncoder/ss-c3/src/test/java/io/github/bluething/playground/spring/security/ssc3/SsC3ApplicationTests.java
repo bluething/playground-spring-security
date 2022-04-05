ab31cd566ac504959260f7d52b3a4f0105339f68
bluething <habib.gunadarma@gmail.com>
2022-04-05 13:27:19 +0700

    Init ss-c3 module

---
+++ spring-security-fundamentals/03-UserDetailsManager-and-PasswordEncoder/ss-c3/src/test/java/io/github/bluething/playground/spring/security/ssc3/SsC3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.security.ssc3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsC3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
